6b21fd473e620ba1a78173e39462638a664a446d
Pascal Hartig <passy@twitter.com>
2014-12-28 18:55:18 +0100

    Make config DRY
    Could use assign() if modifications to the base app are necessary later.

--- config/config.js
+++ config/config.js
@@ -2,30 +2,18 @@ var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
     env = process.env.NODE_ENV || 'development';
 
+var baseApp = {
+  root: rootPath,
+  app: {
+    name: 'a11yapp'
+  },
+  port: 3000,
+};
+
 var config = {
-  development: {
-    root: rootPath,
-    app: {
-      name: 'a11yapp'
-    },
-    port: 3000,
-  },
-
-  test: {
-    root: rootPath,
-    app: {
-      name: 'a11yapp'
-    },
-    port: 3000,
-  },
-
-  production: {
-    root: rootPath,
-    app: {
-      name: 'a11yapp'
-    },
-    port: 3000,
-  }
+  development: baseApp
+  test: baseApp,
+  production: baseApp
 };
 
 module.exports = config[env];
